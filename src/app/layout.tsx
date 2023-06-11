b11443b15aa0668239ebb37ae82b336ed799cc5e
The Count <itapuc7@gmail.com>
2023-06-10 20:37:19 -0400

    Added phone number

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,18 +18,22 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <footer className="bg-[#494949] text-white p-6">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="text-center md:text-left mb-4 md:mb-0">
-          <h2 className="text-2xl font-bold">TimeFix</h2>
-          <p>75 Laurier Ave E, Ottawa, Canada</p>
-        </div>
-        <div>
-          <h3 className="font-semibold">Heures d'ouverture</h3>
-          <p>Lun - Ven: 9:00 - 17:00</p>
-          <p>Sam - Dim: Closed</p>
-        </div>
-      </div>
-    </footer>
+          <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <div className="text-center md:text-left mb-4 md:mb-0">
+              <h2 className="text-2xl font-bold">TimeFix</h2>
+              <p>75 Laurier Ave E, Ottawa, Canada</p>
+            </div>
+            <div className="text-center mb-4 md:mb-0">
+              <h3 className="font-semibold">Contactez-nous</h3>
+              <p>Téléphone: (123) 456-7890</p>
+            </div>
+            <div>
+              <h3 className="font-semibold">Heures d'ouverture</h3>
+              <p>Lun - Ven: 9:00 - 17:00</p>
+              <p>Sam - Dim: Closed</p>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   )
